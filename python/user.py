e873ad151e6f0797fa75792126f312238f684b16
Comfort <agbonguacomfort22@gmail.com>
2020-07-03 11:06:14 +0000

    found

---
+++ python/user.py
@@ -0,0 +1,43 @@
+print("Hello World")
+print("My name is Comfort")
+
+myName = "Comfort" 
+myOccupation = "Software"
+age = 22
+isEmployed = True
+isNotEmployed = False
+isNotFromAfrica = False
+
+
+print("my name is ", myName)
+print("My name is %s I am a %s" % (myName, myOccupation))
+
+dataTypeOfName = type(myName)
+print("The name variable is of %s data type" %(dataTypeOfName))
+print(type(age))
+print(type(isEmployed))
+print(type(isNotFromAfrica))
+
+def sum (first, second):
+    sum = first + second
+    return sum
+
+print(sum(10, 5))
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+ 
+
