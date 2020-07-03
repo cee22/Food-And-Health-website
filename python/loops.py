f07c5f61bed218f86d3ab4229760a19648b866f0
Comfort <agbonguacomfort22@gmail.com>
2020-07-03 11:05:50 +0000

    change

---
+++ python/loops.py
@@ -0,0 +1,22 @@
+from calculate import calculate
+
+number = 1
+
+while number <=20:
+  print("Number: ", number)
+  number = number + 1
+
+
+
+for number in range(1, 20, 2) :
+    print("Number: " , number)
+    
+
+names = ["Comfort", "Ajhoa", "Cee", "Tetteh" ]
+for name in names:
+       print("Name is: ", name)
+
+
+print("Our loop is done!")
+
+
